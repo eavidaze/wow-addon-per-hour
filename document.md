c5e265d79ee5a37674156eb23e76a864361f6f87
JuninZe <juninzeweb@gmail.com>
2021-06-02 01:38:05 -0300

    documenting the folder structure

---
+++ document.md
@@ -0,0 +1,24 @@
+# Folder Strucutre
+
+src / `Init.xml`
+
+- utils / `Init.xml`
+
+  - Utils.lua
+  
+- model / `Init.xml`
+
+  - Model.lua
+  - Settings.lua
+
+- modules / `Init.xml`
+
+  - BaseModule.lua
+  - Honor.lua
+  - Experience.lua
+
+- core / `Init.xml`
+
+  - Displayer.lua
+  - ToggleMenu.lua
+  - CommandLine.lua
--- src/model/Settings.lua
+++ src/model/Settings.lua
@@ -4,10 +4,12 @@ local Utils = Utils
 
 local Settings = PerHour.Settings
 
+-- read the status
 function Settings:IsShown(moduleName)
 	return PerHourAccount_Settings[moduleName].isShown
 end
 
+-- write the status
 function Settings:SetIsShown(moduleName, isShown)
 	PerHourAccount_Settings[moduleName].isShown = isShown
 end
@@ -22,16 +24,18 @@ function Settings:Init()
     end
 
     for key,contextModule in pairs(modules) do
-        local name = contextModule.Name
+        -- for each module i have to initialize if does not exists
+
+        local moduleName = contextModule.Name
         
         -- for each module, we check if the module are set.
-        if (PerHourAccount_Settings[name] == nil) then
-            PerHourAccount_Settings[name] = {}
+        if (PerHourAccount_Settings[moduleName] == nil) then
+            PerHourAccount_Settings[moduleName] = {}
         end
 
         -- for each module registred, we check if the configs are set.
-        if (PerHourAccount_Settings[name].isShown == nil) then
-            self:SetIsShown(name, true)
+        if (PerHourAccount_Settings[moduleName].isShown == nil) then
+            self:SetIsShown(moduleName, true)
         end
     end
 
